783a4eaddb486d4b9455dc79d954a6b2fcb54b12
mh <mh@immerda.ch>
2012-02-16 14:35:31 +0100

    improve setting of value
    If we failed once to set the value, we didn't try to fix the value.
    Also if something else changed the value, puppet wouldn't change it
    back.
    Also the current code only worked on OpenBSD systems, but not on
    any linux systems.

--- manifests/value.pp
+++ manifests/value.pp
@@ -1,14 +1,27 @@
-define sysctl::value(
-	$value
-){
+define sysctl::value (
+  $key = 'name',
+  $value
+) {
 
- 	exec { "exec_sysctl_${name}":
- 		command => "/sbin/sysctl ${name}=\"${value}\"",
- 		refreshonly => true,
- 	}
-
- 	sysctl { $name:
- 		val => "$value",
- 		notify => Exec["exec_sysctl_${name}"],
- 	}
+  $real_key = $key ? {
+    'name' => $name,
+    default => $key
+  }
+  sysctl {
+    $real_key :
+      val => $value,
+      before => Exec["exec_sysctl_${real_key}"],
+  }
+  exec {
+    "exec_sysctl_${real_key}" :
+      command => $::kernel ? {
+        openbsd => "sysctl ${real_key}=\"${value}\"",
+        default => "sysctl -w ${real_key}=\"${value}\""
+      },
+      unless => $::kernel ? {
+        openbsd => "sysctl ${real_key} | grep -q '=${value}'",
+        default => "sysctl ${real_key} | grep -q ' = ${value}'"
+      },
+      require => Sysctl[$real_key],
+  }
 }
