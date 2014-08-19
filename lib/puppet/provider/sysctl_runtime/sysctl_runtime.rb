3108f2568148adfd006638a6fa7403e30a347490
Artem Sidorenko <artem@2realities.com>
2014-08-19 15:34:26 +0200

    Fixed the sysctl in case of stderr errors
    In case of errors and combine of stderr&stdout output of sysctl
    the output gets broken and can't be parsed:
    
    kernel.shmmni = 4096
    kernel.shmerror: permission denied on key 'vm.compact_memory'
    _rmid_forced = 0
    kernel.msgmax = 8192
    --
    net.ipv4.route.gc_min_interval = 0
    net.iperror: permission denied on key 'net.ipv4.route.flush'
    v4.route.gc_min_interval_ms = 500
    --
    net.ipv6.conf.default.max_desync_factor = 600
    net.ipv6.conf.default.max_addresses =error: permission denied on key 'net.ipv6.route.flush'
     16
    net.ipv6.conf.default.accept_ra_defrtr = 1

--- lib/puppet/provider/sysctl_runtime/sysctl_runtime.rb
+++ lib/puppet/provider/sysctl_runtime/sysctl_runtime.rb
@@ -9,6 +9,7 @@ Puppet::Type.type(:sysctl_runtime).provide(:sysctl_runtime,
   "/usr/bin/sysctl"].each do |sysctl_command|
     if File.exists?(sysctl_command)
       commands :sysctl => sysctl_command
+      @sysctl_exec_command = sysctl_command
       break
     end
   end
@@ -16,7 +17,17 @@ Puppet::Type.type(:sysctl_runtime).provide(:sysctl_runtime,
   mk_resource_methods
 
   def self.instances
-    sysctl('-a').split("\n").collect do |line|
+    #we don't use here the sysctl command to be able 
+    #to disable combining of stderr/stdout output. 
+    #Sysctl produces mixed output in case of error, 
+    #and it can't be parsed.
+    #https://ask.puppetlabs.com/question/6299/combine-false-for-provider-commands/
+    output = Puppet::Util.execute(@sysctl_exec_command+" -a", {
+      :failonfail         => true,
+      :combine            => false,
+      :custom_environment => {}
+    })
+    output.split("\n").collect do |line|
       name, val = line.split(' = ')
       new( 
         :name => name,
