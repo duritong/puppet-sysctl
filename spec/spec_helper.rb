42340de8752466009d3565802aa2541d3a380eac
mh <mh@immerda.ch>
2015-06-05 12:25:21 +0200

    make tests work with puppet >= 4.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,6 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.environmentpath = File.join(Dir.pwd, 'spec')
   c.alias_it_should_behave_like_to(:it_configures, 'configures')
 end
